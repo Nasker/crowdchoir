e0b25f3523c7198cee289ffd8608fc7939ae1225
Nasker <xamps23@gmail.com>
2024-09-19 10:56:41 +0200

    Work on getting the websockets working, at least it says it connects...

--- static/Synth.js
+++ static/Synth.js
@@ -1,12 +1,7 @@
 export default class Synth {
     constructor() {
-        // Create a single Tone.Synth object
         this.synth = new Tone.Synth();
-
-        // Create a feedback delay effect
         this.feedbackDelay = new Tone.FeedbackDelay("8n", 0.5).toDestination();
-
-        // Create a lowpass filter
         this.filter = new Tone.Filter({
             type: 'lowpass',
             frequency: 350,
@@ -15,18 +10,18 @@ export default class Synth {
 
         this.synth.connect(this.filter);
         this.filter.connect(this.feedbackDelay);
+        this.bluesScale = ["C4", "Eb4", "F4", "F#4", "G4", "Bb4", "C5"];
+        this.minFreq = Math.log(20);
+        this.maxFreq = Math.log(20000);
     }
 
     playNoteFromPosition(x, y) {
-        const bluesScale = ["C4", "Eb4", "F4", "F#4", "G4", "Bb4", "C5"];
-        const noteIndex = Math.floor((x / window.innerWidth) * bluesScale.length);
-        const note = bluesScale[noteIndex];
-        const minFreq = Math.log(20);
-        const maxFreq = Math.log(20000);
+        const noteIndex = Math.floor((x / window.innerWidth) * this.bluesScale.length);
+        const note = this.bluesScale[noteIndex];
         const scaleY = 1 - (y / window.innerHeight);
-        const frequency = Math.exp(scaleY * (maxFreq - minFreq) + minFreq);
+        const frequency = Math.exp(scaleY * (this.maxFreq - this.minFreq) + this.minFreq);
         this.filter.frequency.value = frequency;
-        const time = Tone.now();
-        this.synth.triggerAttackRelease(note, "8n", time);
+        this.synth.triggerRelease();
+        this.synth.triggerAttackRelease(note, "64n");
     }
 }
