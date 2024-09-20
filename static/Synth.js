ba5ed0b97d3e1b5d6c61148a1eb41f5f35b39060
Nasker <xamps23@gmail.com>
2024-09-20 17:00:19 +0200

    Works and through the network on several devices connected

--- static/Synth.js
+++ static/Synth.js
@@ -1,13 +1,21 @@
 export default class Synth {
     constructor(musicController) {
         this.musicController = musicController;
-        this.synth = new Tone.Synth();
+        this.synth = new Tone.Synth({
+            envelope: {
+                attack: 2.0,  // Time it takes to reach full volume
+                decay: 0.2,   // Time it takes to drop to sustain level
+                sustain: 1.0, // Sustain level (as a percentage of full volume)
+                release: 2.0  // Time it takes to fade out after the note is released
+            }
+        });
         this.feedbackDelay = new Tone.FeedbackDelay("8n", 0.5).toDestination();
         this.filter = new Tone.Filter({
             type: 'lowpass',
             frequency: 350,
             Q: 1
         });
+
         this.synth.connect(this.filter);
         this.filter.connect(this.feedbackDelay);
         this.minFreq = Math.log(20);
--- static/main.js
+++ static/main.js
@@ -17,10 +17,9 @@ function processSocketData(data) {
     musicController.set_current_chord(data.value)
     console.log(musicController.get_current_root_note_name());
     console.log(musicController.get_chord_name());
-    //get a random x and y value between the window width and height
-    //const x = Math.random() * window.innerWidth;
-    //const y = Math.random() * window.innerHeight;
-    //synth.playNoteFromPosition(x, y);
+    const x = Math.random() * window.innerWidth;
+    const y = Math.random() * window.innerHeight;
+    synth.playNoteFromPosition(x, y);
 }
 
 const webSocketHandler = new WebSocketHandler(`http://${window.location.hostname}:5000`,
