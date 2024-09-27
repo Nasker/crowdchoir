a412340b12cfed67c50ab1c004e6a78e9c6a8a72
Nasker <xamps23@gmail.com>
2024-09-27 16:50:30 +0200

    HarmonyBridge with ChordFinder now, working!!!

--- HarmonyBridge.py
+++ HarmonyBridge.py
@@ -1,5 +1,6 @@
 import mido
-
+from ChordFinder import ChordFinder
+from MusicController import RTPMusicController
 class HarmonyBridge:
     """
     A class that listens to a MIDI Virtual port and sends CommandControl structs to the callee using a callback.
@@ -15,23 +16,57 @@ class HarmonyBridge:
         mido.set_backend('mido.backends.rtmidi')
         print("Available MIDI input ports:", mido.get_input_names())
         self.port_name = port_name
+        self.played_notes = []
+        self.channel = 1
+        self.len_detected_chord = 4
         try:
-            self.port = mido.open_input(self.port_name, callback=self.on_control_change)
+            self.port = mido.open_input(self.port_name, callback=self.select_message_type)
         except IOError:
             print(f"Could not open MIDI input port: {self.port_name}")
             exit(1)
         self.callback = callback
         HarmonyBridge.instance_count += 1  # Increment the instance count when a new instance is created
         print(f"Number of HarmonyBridge instances: {HarmonyBridge.instance_count}")
+        self.music_controller = RTPMusicController()
+        self.chord_finder = ChordFinder(self.music_controller)
+
+    def select_message_type(self, message):
+        """
+        Selects the appropriate callback function based on the MIDI message type.
+        :param message: The MIDI message received.
+        """
+        if message.type == 'control_change':
+            self.on_control_change(message)
+        elif message.type == 'note_on':
+            self.on_note_on(message)
+        elif message.type == 'note_off':
+            self.on_note_off(message)
 
     def on_control_change(self, message):
         """
         Callback function for the MIDI input port.
         :param message: The MIDI message received.
         """
-        if message.type == 'control_change':
-            print(f"Received MIDI message: {message}")
-            self.callback(message.control, message.value)
+        print(f"Control Change: {message.control} Value: {message.value}")
+        self.callback(message.control, message.value)
+
+    def on_note_on(self, message):
+        """
+        Callback function for the MIDI input port.
+        :param message: The MIDI message received.
+        """
+        if message.channel == self.channel:
+            self.played_notes.append(message.note)
+            if len(self.played_notes) == self.len_detected_chord:
+                print("--->",self.chord_finder.identify_chord(self.played_notes))
+
+    def on_note_off(self, message):
+        """
+        Callback function for the MIDI input port.
+        :param message: The MIDI message received.
+        """
+        if message.channel == self.channel:
+            self.played_notes.remove(message.note)
 
     def close(self):
         """
@@ -46,6 +81,7 @@ if __name__ == '__main__':
 
     # Open a virtual port and start listening
     bridge = HarmonyBridge('CHORDION_MIDI Port 1', callback)
+    # bridge = HarmonyBridge('UMX 25', callback)
 
     try:
         input("Listening for MIDI control changes. Press Enter to exit...\n")
